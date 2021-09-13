9c273066b0d363b4a5fa73fa6a8bb5aa6cbfb1d1
Jason <74387813+jusong23@users.noreply.github.com>
2021-09-13 17:46:53 +0900

    *#4.0 Fetching Movies from API
    1. what is 'this' ?
    2. state 의 정체

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
-
+import axios from "axios";
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
+  }; //변하는 data 넣기위해 , functoion component 대신 state가 작동되는 class component 사용
+
+  getMovies = async () => {
+    const movies = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+    );
   };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
+
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading. . . " : "We are ready"}</div>;
+    const { isLoading } = this.state; // class component 이기에 this.state 사용
+    // state의 사명을 이루어 주고자, 윗 줄과 같은 과정을 통해 state의 key를 바꿔줌
+    return <div> {isLoading ? "Loading ..." : "we are ready"}</div>;
   }
 }
 
